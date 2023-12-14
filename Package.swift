ba84cb219da099774b4261de433c2d03b828ea51
Alan DeGuzman <alandeguz@users.noreply.github.com>
2023-12-14 10:32:51 -0600

    update to 0.54.0

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
       dependencies: ["SwiftLintBinary"]),
     .binaryTarget(
       name: "SwiftLintBinary",
-      url: "https://github.com/realm/SwiftLint/releases/download/0.51.0/SwiftLintBinary-macos.artifactbundle.zip",
-      checksum: "9fbfdf1c2a248469cfbe17a158c5fbf96ac1b606fbcfef4b800993e7accf43ae"
+      url: "https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip",
+      checksum: "963121d6babf2bf5fd66a21ac9297e86d855cbc9d28322790646b88dceca00f1"
     )
   ]
 )
